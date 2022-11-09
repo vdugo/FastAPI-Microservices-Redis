675a681f6e43016e68e3a572eee9896554e55982
Vincent <v3rdugo@yahoo.com>
2022-11-08 17:38:14 -0800

    use environment variables instaed of hardcoded values

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -8,4 +14,11 @@ app.add_middleware(
     allow_origins=['http://localhost:3000'],
     allow_methods=['*'],
     allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host=os.getenv('REDIS_HOST'),
+    port=os.getenv('REDIS_PORT'),
+    password=os.getenv('REDIS_PASSWORD'),
+    decode_responses=True
 )
