8823bc7d95762769a1f31d472c30ceb9c192bb05
Vincent <v3rdugo@yahoo.com>
2022-11-09 13:34:10 -0800

    reduce the quantity and update the product once an order is fulfilled

--- fulfillment.py
+++ fulfillment.py
@@ -15,6 +15,13 @@ while True:
     try:
         results = redis.xreadgroup(groupname=group, consumername=key, streams={key: '>'})
         print(results)
+        
+        if results != []:
+            for result in results:
+                obj = result[1][0][1]
+                product = Product.get(obj['product_id'])
+                product.quantity -= int(obj['quantity'])
+                product.save()
     except Exception as e:
         print(str(e))
     time.sleep(3)
